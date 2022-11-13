009ea2fbef9beb95d1be758c4fbca9aa3dc159a3
crislerwin <crisler.wintler@splitc.com.br>
2022-11-13 13:43:10 -0300

    ✅ update order  and test cases

--- internal/order/entity/order.go
+++ internal/order/entity/order.go
@@ -21,5 +21,20 @@ func (o *Order) isValid() error {
 	if o.ID == "" {
 		return errors.New("invalid id")
 	}
+	if o.Price <= 0 {
+		return errors.New("invalid price")
+	}
+	if o.Tax <= 0 {
+		return errors.New("invalit tax")
+	}
+	return nil
+}
+
+func (o *Order) CalculateFinalPrice() error {
+	o.FinalPrice = o.Price + o.Tax
+	err := o.isValid()
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- internal/order/entity/order_test.go
+++ internal/order/entity/order_test.go
@@ -11,3 +11,31 @@ func TestGivenAnEmptyId_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.
 	assert.Error(t, order.isValid(), "invalid id")
 
 }
+
+func TestGivenAnEmptyPrice_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := Order{ID: "123"}
+	assert.Error(t, order.isValid(), "invalid price")
+
+}
+
+func TestGivenAnEmptyTax_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := Order{ID: "123", Price: 10}
+	assert.Error(t, order.isValid(), "invalid tax")
+
+}
+
+func TestGivenAValidParams_WhenICallNewOrder_ThenIShouldReceiveCreateOrderWithAllParams(t *testing.T) {
+	order := Order{
+		ID:    "123",
+		Price: 10.2,
+		Tax:   2.0,
+	}
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.2, order.Price)
+	assert.Equal(t, 2.0, order.Tax)
+	assert.Nil(t, order.isValid())
+}
+
+func GivenAPriceAndTax_WhenICallCalculatePrice_ThenIShouldSetFinalPrice(t *testing.T) {
+
+}
