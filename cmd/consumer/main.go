d4cec8a2d15d423dd82e6f4d7a3057cd0a28b846
crislerwin <crisler.wintler@splitc.com.br>
2022-11-14 21:28:45 -0300

    🚀 reading rabbitmq messages

---
+++ cmd/consumer/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/crislerwin/order-app/internal/order/entity/infra/database"
+	"github.com/crislerwin/order-app/internal/order/usecase"
+	"github.com/crislerwin/order-app/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	// sqlite3
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	repository := database.NewOrderRepository(db)
+	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: repository}
+
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
+
+	for msg := range out {
+		var inputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &inputDTO)
+		if err != nil {
+			panic(err)
+		}
+		outputDTO, err := uc.Execute(inputDTO)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Println(outputDTO)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
